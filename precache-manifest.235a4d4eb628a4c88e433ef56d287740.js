0178eacafc9a5f6a572ef06c9bc83e43c6686a45
荣晓红 <44945323+xiaoixao@users.noreply.github.com>
2020-10-04 20:42:23 +0800

    add

---
+++ precache-manifest.235a4d4eb628a4c88e433ef56d287740.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ec4802a0b5c79d7f79ee9528e99b28c3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "fd2574f1652b285f5c2b",
+    "url": "/static/js/2.f8e35e53.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.f8e35e53.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b1af8f6cab54a032eebb",
+    "url": "/static/js/main.2da21492.chunk.js"
+  },
+  {
+    "revision": "e2702865a21868f2ebe6",
+    "url": "/static/js/runtime-main.7918a0be.js"
+  }
+]);
